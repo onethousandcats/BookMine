c84229a803c561628e815be0c3bb2a7039a637bc
onethousandcats <phillip.bercot@gmail.com>
2013-09-26 10:12:53 -0400

    first

---
+++ Models/Model.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookMine.Models
+{
+    public class Model
+    {
+
+        public class Author
+        {
+            public int Id { get; set; }
+            
+            //bio
+            [Required]
+            public string LastName { get; set; }
+            [Required]
+            public string FirstName { get; set; }
+
+            [DisplayFormat(DataFormatString = "{0:M-d-yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime Birth { get; set; }
+            [DisplayFormat(DataFormatString = "{0:M-d-yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime Death { get; set; }
+
+            public virtual Country Nationality { get; set; }
+
+        }
+
+        public class Book
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public string Title { get; set; }
+            [DisplayFormat(DataFormatString = "{0:M-d-yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime Published { get; set; }
+          
+        }
+
+        public class Word
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public string Word { get; set; }
+
+        }
+
+        public class Country
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+        }
+    }
+}
